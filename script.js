b0c789862caf5ae0652e093824cc6a1e09edc499
Tamim-Nekaien <tnekaien@ucdavis.edu>
2019-09-29 12:45:26 -0700

    added favicon functionality and bootstrap 4

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict';
 
+alert("Warning: This webpage will kick you out in 15 minutes, hurry up!");
+
 function display() {
+alert("Hello World");
+  prompt('hello');
+console.log('yo');
+  document.getElementById("john").innerHTML="yo what's up?";
 
 }
